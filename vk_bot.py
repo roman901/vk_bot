4a07b4ad7019a50e21bd433c7cb35277edf69920
Roman Shishkin <spark@uwtech.org>
2017-08-25 13:15:44 +0200

    Change start message

--- vk_bot.py
+++ vk_bot.py
@@ -2,6 +2,6 @@ import os
 from import_string.base import import_string
 
 config_file = os.environ.get('VK_BOT_CONFIG', 'local_config.LocalConfig')
-print('Using {}'.format(config_file))
+print('Start using {}'.format(config_file))
 
 obj = import_string(config_file)
