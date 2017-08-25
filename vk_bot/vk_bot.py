9e9ee22a5ab89e8ed18ab98e66b61dbde2767f83
Roman Shishkin <spark@uwtech.org>
2017-08-25 19:42:09 +0200

    Simple plugin load mechanism

--- vk_bot/vk_bot.py
+++ vk_bot/vk_bot.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from .config import Config
 
 
@@ -6,16 +7,23 @@ class VKBot(object):
     def __init__(self):
         self.config = Config()
         self.logger = logging.getLogger()
+        self.plugins = []
 
     def run(self):
         self.init_logging()
         self.logger.info('Starting VKBot')
 
+        self.logger.info('Init plugins')
+        self.init_plugins()
+        self.logger.info('Loaded {} plugins'.format(len(self.plugins)))
+
     def init_logging(self):
         self.logger.setLevel(logging.INFO)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
         sh = logging.StreamHandler()
         sh.setFormatter(formatter)
+        self.logger.addHandler(sh)
 
         if self.config['DEBUG']:
             self.logger.setLevel(logging.DEBUG)
@@ -25,5 +33,9 @@ class VKBot(object):
             fh.setFormatter(formatter)
             self.logger.addHandler(fh)
 
-        self.logger.addHandler(sh)
-
+    def init_plugins(self):
+        plugins = os.listdir('plugins')
+        for p in plugins:
+            if p.endswith('.py') and not p.startswith('.'):
+                self.logger.debug('Found plugin {}'.format(p))
+                self.plugins.append(p)
