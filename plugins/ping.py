331acef5d8b201e9a84d1ec1e79fdbaeb8c66988
Roman Shishkin <spark@uwtech.org>
2017-08-26 11:40:50 +0200

    Register ping command

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,3 +1,8 @@
 class Plugin:
     def __init__(self, vk_bot):
         self.vk_bot = vk_bot
+        self.vk_bot.add_command('ping', self.ping)
+
+    def ping(self):
+        # TODO(spark): Answer pong to VK API
+        print('pong')
