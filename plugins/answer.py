bbcbb2127437814cf61d9e7dddbbfb4d35ef9415
Roman Shishkin <spark@uwtech.org>
2017-08-26 13:33:59 +0200

    Add answer plugin

--- plugins/answer.py
+++ plugins/answer.py
@@ -1,4 +1,8 @@
 class Plugin:
     def __init__(self, vk_bot):
         self.vk_bot = vk_bot
-        print('Test plugin loaded')
+        self.vk_bot.add_filter(self.answer)
+
+    async def answer(self, sender, message):
+        if message.endswith('нет'):
+            await self.vk_bot.send_message(sender, 'Пидора ответ')
