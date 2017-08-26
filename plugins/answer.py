5fe91a7bc51c1263002378d4542ec8eb228e7184
Roman Shishkin <spark@uwtech.org>
2017-08-26 21:11:05 +0200

    Remove answer filter

--- plugins/answer.py
+++
@@ -1,8 +0,0 @@
-class Plugin:
-    def __init__(self, vk_bot):
-        self.vk_bot = vk_bot
-        self.vk_bot.add_filter(self.answer)
-
-    async def answer(self, sender, sender_id, message):
-        if message.endswith('нет') or message.endswith('Нет'):
-            await self.vk_bot.send_message(sender, 'Пидора ответ')
